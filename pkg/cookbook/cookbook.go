41309551766ed16969854b7bca01b99ad4eea1d6
Jim Rosser <jarosser06@gmail.com>
2014-11-15 21:54:01 -0600

    added GenFiles

--- pkg/cookbook/cookbook.go
+++ pkg/cookbook/cookbook.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"text/template"
 	"time"
+
+	"github.com/GeertJohan/go.rice"
 )
 
 type OSTarget struct {
@@ -22,13 +25,45 @@ type Cookbook struct {
 	Year         int
 }
 
-func NewCookbook(path string) Cookbook {
+func NewCookbook(cookbookPath string, name string) Cookbook {
 	cookbook := Cookbook{
 		Year: time.Now().Year(),
-		Path: path,
+		Path: path.Join(cookbookPath, name),
+		Name: name,
 	}
 
 	return cookbook
+}
+
+func (c *Cookbook) GenFiles() error {
+
+	cookbookFiles := [8]string{
+		"Berksfile",
+		"CHANGELOG.md",
+		"Gemfile",
+		"metadata.rb",
+		"README.md",
+		"recipes/default.rb",
+		"test/unit/spec/default_spec.rb",
+		"test/unit/spec/spec_helper.rb",
+	}
+
+	templateBox, _ := rice.FindBox("templates")
+	for _, cookbookFile := range cookbookFiles {
+		tmpStr, _ := templateBox.String(cookbookFile)
+		t, _ := template.New(cookbookFile).Parse(tmpStr)
+
+		f, err := os.Create(path.Join(c.Path, cookbookFile))
+		if err != nil {
+			return errors.New(fmt.Sprintf("cookbook.GenFiles(): %v", err))
+		}
+
+		// TODO: Need to clean up newlines before the file gets written
+		t.Execute(f, c)
+		f.Close()
+	}
+
+	return nil
 }
 
 func (c *Cookbook) GenDirs() error {
