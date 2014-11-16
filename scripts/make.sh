5ddb0c6996ac2abb4dccb808559afc4048494718
Jim Rosser <jarosser06@gmail.com>
2014-11-15 21:24:00 -0600

    fixed the testing script

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+pkg_dirs="util cookbook config"
+
 case $1 in
 "test")
-  pushd fastfood &> /dev/null
-  go test
-  popd &> /dev/null
-  pushd fastfood/cookbook &> /dev/null
-  go test
-  popd &> /dev/null
+  for dir in $pkg_dirs
+  do
+    pushd pkg/${dir} &> /dev/null
+    go test
+    popd &> /dev/null
+  done
   ;;
 "build")
   mkdir -p ${GOPATH}/src/github.com/jarosser06
