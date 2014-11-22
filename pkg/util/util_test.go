a74981eaadf5a7275e5c96dae8dcdb33c7549173
Jim Rosser <jarosser06@gmail.com>
2014-11-22 11:06:09 -0600

    wrote test to attempting to reproduce error with clean not working

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -19,6 +19,19 @@ func TestCollapseNewlines(t *testing.T) {
 	}
 }
 
+func TestCollapseNewlines_catches_all(t *testing.T) {
+	testString := "include_recipe 'nginx'\n\n\ninclude_recipe' 'apache'\n\n\n\n\npackage foo"
+
+	res := CollapseNewlines(testString)
+	if strings.Contains(res, "\n\n\n") {
+		t.Errorf("Expected resulting string not to contain 3 newlines")
+	}
+
+	if strings.Contains(res, "\n\n\n\n") {
+		t.Errorf("Expected resulting string to not contain 4 newlines")
+	}
+}
+
 func TestIsNodeAttr(t *testing.T) {
 	testMatch := "node['cookbook']['attr']"
 	testNotMatch := "something[diff]"
