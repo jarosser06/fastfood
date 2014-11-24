14f915539a33f688332b37ba751dd9e5db3b33d5
Jim Rosser <jarosser06@gmail.com>
2014-11-23 19:26:55 -0600

    fixed typo

--- provider/templates/application/test/unit/spec/application_spec.rb
+++ provider/templates/application/test/unit/spec/application_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'spec_helper'
 
-descript '|{ .Cookbook.Name }|::|{ .Name }|' do
+describe '|{ .Cookbook.Name }|::|{ .Name }|' do
   let(:chef_run) do
     ChefSpec::Runner.new.converge('|{ .Cookbook.Name }|::|{ .Name }|')
   end
