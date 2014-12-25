80969ad1f08ef45894645f11ba3eb664ae9cdc03
Jim Rosser <jarosser06@gmail.com>
2014-12-24 20:13:38 -0600

    new alpha build

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 export GOPATH := ${PWD}/.gopath
-export FFVERSION := 0.2.0alpha1
+export FFVERSION := 0.2.0alpha2
 export INSTALLPRE := /usr/local
 
 all: deps build
--- cmd/main/main.go
+++ cmd/main/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/mitchellh/cli"
 )
 
-const ffVersion = "0.2.0alpha1"
+const ffVersion = "0.2.0alpha2"
 
 func main() {
 	c := cli.NewCLI("fastfood", ffVersion)
