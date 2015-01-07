c83a19a4fa49623b3bdfc3ead6eba457955f6a94
Jim Rosser <jarosser06@gmail.com>
2015-01-07 15:01:49 -0600

    empty

--- framework/chef/berks.go
+++ framework/chef/berks.go
@@ -1,7 +1,31 @@
 package chef
 
+import (
+	"io"
+	"os"
+)
+
 type Berks struct {
 	Git      string `json:"git"`
 	Path     string `json:"path"`
 	Revision string `json:"revision"`
+}
+
+// Read a berkshelf file and return a berks struct
+func BerksFromFile(f string) (Berks, error) {
+	var b Berks
+
+	r, err := os.Open(f)
+	if err != nil {
+		return b, err
+	}
+	defer r.Close()
+
+	b.Parse(r)
+
+	return b, nil
+}
+
+// Parse the Berksfile
+func (b *Berks) Parse(r io.Reader) {
 }
